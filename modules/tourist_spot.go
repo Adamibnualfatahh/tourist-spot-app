5f02b6b6cad15471218fca73e3286caa63e3b20a
adamibnu <adamibnu157@gmail.com>
2024-05-15 19:14:29 +0700

    enhance(tourist-spot): create searchBy

--- modules/tourist_spot.go
+++ modules/tourist_spot.go
@@ -1,11 +1,13 @@
 package modules
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"os/exec"
 	"runtime"
 	"sort"
+	"strings"
 )
 
 var (
@@ -48,7 +50,7 @@ func MainMenu() {
 	case 2:
 		LoginUser()
 	case 3:
-		fmt.Println("Anda memilih Cari Tempat Wisata.")
+		SearchTouristSpots()
 	case 4:
 		GetAllTouristSpots()
 	case 5:
@@ -119,5 +121,203 @@ func GetAllTouristSpots() {
 		}
 
 		fmt.Println()
+	}
+}
+
+func SearchTouristSpots() {
+	fmt.Println("=== Pencarian Tempat Wisata ===")
+	fmt.Println("1. Berdasarkan Nama")
+	fmt.Println("2. Berdasarkan Lokasi")
+	fmt.Println("3. Berdasarkan Wahana")
+	fmt.Println("4. Berdasarkan Fasilitas")
+	fmt.Println("5. Kembali ke Menu Utama")
+
+	var choice int
+	fmt.Print("Masukkan pilihan Anda: ")
+	fmt.Scanln(&choice)
+
+	switch choice {
+	case 1:
+		SearchByName()
+	case 2:
+		SearchByLocation()
+	case 3:
+		SearchByAttraction()
+	case 4:
+		SearchByFacility()
+	case 5:
+		fmt.Println("Kembali ke Menu Utama.")
+		MainMenu()
+	default:
+		fmt.Println("Pilihan tidak valid. Mohon masukkan opsi yang benar.")
+		SearchTouristSpots()
+	}
+}
+
+func SearchByName() {
+	fmt.Print("Masukkan nama tempat wisata yang ingin dicari: ")
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		query := scanner.Text()
+
+		found := false
+		for _, touristSpot := range TouristSpotDB {
+			if strings.Contains(strings.ToLower(touristSpot.Name), strings.ToLower(query)) {
+				fmt.Println("ID:", touristSpot.Id)
+				fmt.Println("Nama:", touristSpot.Name)
+				fmt.Println("Lokasi:", touristSpot.Location)
+				fmt.Println("Kategori:", touristSpot.Category)
+				fmt.Println("Deskripsi:", touristSpot.Description)
+
+				fmt.Println("Wahana:")
+				for _, attraction := range AttractionDB {
+					if attraction.TouristSpotId == touristSpot.Id {
+						fmt.Println("  - Nama Atraksi:", attraction.Name)
+						fmt.Println("    Deskripsi:", attraction.Description)
+					}
+				}
+
+				fmt.Println("Fasilitas:")
+				for _, facility := range FacilityDB {
+					if facility.TouristSpotId == touristSpot.Id {
+						fmt.Println("  - Nama Fasilitas:", facility.Name)
+						fmt.Println("    Deskripsi:", facility.Description)
+					}
+				}
+				found = true
+			}
+		}
+
+		if !found {
+			fmt.Println("Tempat wisata dengan nama tersebut tidak ditemukan.")
+		}
+	}
+
+	fmt.Println()
+	MainMenu()
+}
+
+func SearchByLocation() {
+	fmt.Print("Masukkan lokasi tempat wisata yang ingin dicari: ")
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		query := scanner.Text()
+
+		found := false
+		for _, touristSpot := range TouristSpotDB {
+			if strings.Contains(strings.ToLower(touristSpot.Location), strings.ToLower(query)) {
+				fmt.Println("ID:", touristSpot.Id)
+				fmt.Println("Nama:", touristSpot.Name)
+				fmt.Println("Lokasi:", touristSpot.Location)
+				fmt.Println("Kategori:", touristSpot.Category)
+				fmt.Println("Deskripsi:", touristSpot.Description)
+
+				fmt.Println("Wahana:")
+				for _, attraction := range AttractionDB {
+					if attraction.TouristSpotId == touristSpot.Id {
+						fmt.Println("  - Nama Atraksi:", attraction.Name)
+						fmt.Println("    Deskripsi:", attraction.Description)
+					}
+				}
+
+				fmt.Println("Fasilitas:")
+				for _, facility := range FacilityDB {
+					if facility.TouristSpotId == touristSpot.Id {
+						fmt.Println("  - Nama Fasilitas:", facility.Name)
+						fmt.Println("    Deskripsi:", facility.Description)
+					}
+				}
+				found = true
+			}
+		}
+
+		if !found {
+			fmt.Println("Tempat wisata dengan lokasi tersebut tidak ditemukan.")
+		}
+	}
+
+	fmt.Println()
+	MainMenu()
+}
+
+func SearchByAttraction() {
+	fmt.Print("Masukkan wahana wisata yang ingin dicari: ")
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		query := scanner.Text()
+
+		found := false
+		for _, attraction := range AttractionDB {
+			if strings.Contains(strings.ToLower(attraction.Name), strings.ToLower(query)) {
+				touristSpot := TouristSpotDB[attraction.TouristSpotId]
+				fmt.Println("ID:", touristSpot.Id)
+				fmt.Println("Nama:", touristSpot.Name)
+				fmt.Println("Lokasi:", touristSpot.Location)
+				fmt.Println("Kategori:", touristSpot.Category)
+				fmt.Println("Deskripsi:", touristSpot.Description)
+
+				fmt.Println("Wahana:")
+				for _, attraction := range AttractionDB {
+					if attraction.TouristSpotId == touristSpot.Id {
+						fmt.Println("  - Nama Atraksi:", attraction.Name)
+						fmt.Println("    Deskripsi:", attraction.Description)
+					}
+				}
+
+				fmt.Println("Fasilitas:")
+				for _, facility := range FacilityDB {
+					if facility.TouristSpotId == touristSpot.Id {
+						fmt.Println("  - Nama Fasilitas:", facility.Name)
+						fmt.Println("    Deskripsi:", facility.Description)
+					}
+				}
+				found = true
+			}
+		}
+
+		if !found {
+			fmt.Println("Tempat wisata dengan wahana tersebut tidak ditemukan.")
+		}
+	}
+}
+
+func SearchByFacility() {
+	fmt.Print("Masukkan fasilitas wisata yang ingin dicari: ")
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		query := scanner.Text()
+
+		found := false
+		for _, facility := range FacilityDB {
+			if strings.Contains(strings.ToLower(facility.Name), strings.ToLower(query)) {
+				touristSpot := TouristSpotDB[facility.TouristSpotId]
+				fmt.Println("ID:", touristSpot.Id)
+				fmt.Println("Nama:", touristSpot.Name)
+				fmt.Println("Lokasi:", touristSpot.Location)
+				fmt.Println("Kategori:", touristSpot.Category)
+				fmt.Println("Deskripsi:", touristSpot.Description)
+
+				fmt.Println("Wahana:")
+				for _, attraction := range AttractionDB {
+					if attraction.TouristSpotId == touristSpot.Id {
+						fmt.Println("  - Nama Atraksi:", attraction.Name)
+						fmt.Println("    Deskripsi:", attraction.Description)
+					}
+				}
+
+				fmt.Println("Fasilitas:")
+				for _, facility := range FacilityDB {
+					if facility.TouristSpotId == touristSpot.Id {
+						fmt.Println("  - Nama Fasilitas:", facility.Name)
+						fmt.Println("    Deskripsi:", facility.Description)
+					}
+				}
+				found = true
+			}
+		}
+
+		if !found {
+			fmt.Println("Tempat wisata dengan fasilitas tersebut tidak ditemukan.")
+		}
 	}
 }
